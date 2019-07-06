ba00c9e699d4c1b177f2d88dafafc6c57a045276
ThinhPham <thinhphse62039@gmail.com>
2019-07-07 00:16:35 +0700

    import cros

--- src/main/java/com/prcbadminton/badminton/BadmintonApplication.java
+++ src/main/java/com/prcbadminton/badminton/BadmintonApplication.java
@@ -2,6 +2,10 @@ package com.prcbadminton.badminton;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class BadmintonApplication {
@@ -10,4 +14,13 @@ public class BadmintonApplication {
         SpringApplication.run(BadmintonApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:81").allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 }

b2528514865bbc03bf88c557cba476970b16c44c
ThinhPham <thinhphse62039@gmail.com>
2019-07-16 01:27:33 +0700

    CURD producer and promotion

--- src/main/java/com/prcbadminton/badminton/constant/SecurityConstant.java
+++ src/main/java/com/prcbadminton/badminton/constant/SecurityConstant.java
@@ -2,7 +2,7 @@ package com.prcbadminton.badminton.constant;
 
 public class SecurityConstant {
     public static final String SECRET = "badmintonprojectcloud";
-    public static long EXPIRATION_TIME = 864000;
+    public static long EXPIRATION_TIME = 864000000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String AUTHORITIES_KEY = "role";
